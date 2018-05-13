f0133172564da4981f4e505403d851de33a96f9c
Zedizded <zedmatt@live.fr>
2018-05-13 16:52:28 +0200

    Article/Edition - Vue et controller simple

--- src/Zcomdezign/PlatformBundle/Controller/PlatformController.php
+++ src/Zcomdezign/PlatformBundle/Controller/PlatformController.php
@@ -4,18 +4,25 @@ namespace Zcomdezign\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Zcomdezign\PlatformBundle\Entity\Article;
 use Zcomdezign\PlatformBundle\Entity\User;
 use Zcomdezign\PlatformBundle\Form\ArticleType;
 
 class PlatformController extends Controller
-{
+{	
+	/**
+     * @Route("/", name="zcomdezign_platform_homepage")
+     */
     public function indexAction()
 	{
 		return $this->render('ZcomdezignPlatformBundle:Default:index.html.twig');
 	}
-
-	public function creationAction(Request $request)
+    
+    /**
+     * @Route("/creation", name="zcomdezign_platform_creation")
+     */
+    public function creationAction(Request $request)
 	{
 		$article = new Article();
 		$form   = $this->get('form.factory')->create(ArticleType::class, $article);
@@ -35,6 +42,30 @@ class PlatformController extends Controller
 		}
 
 		return $this->render('ZcomdezignPlatformBundle:Default:creation.html.twig', array(
+			'form' => $form->createView(),
+			));
+	}
+    
+    /**
+     * @Route("/edition/{id}", name="zcomdezign_platform_edition")
+     */
+	public function editionAction(Request $request,article $article, $id)
+	{
+		
+		$form   = $this->get('form.factory')->create(ArticleType::class, $article);
+
+		if ($request->isMethod('POST')) {
+			
+			$form->handleRequest($request);
+
+			if ($form->isValid()) {
+
+				$em = $this->getDoctrine()->getManager();
+				$em->flush();
+			}
+		}
+
+		return $this->render('ZcomdezignPlatformBundle:Default:edition.html.twig', array(
 			'form' => $form->createView(),
 			));
 	}
